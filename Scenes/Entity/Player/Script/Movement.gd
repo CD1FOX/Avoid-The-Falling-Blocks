7658099ee085ddeb79a22680817f24f85ba0f570
Francis Leo <drypasta34@gmail.com>
2025-05-17 14:05:59 +0800

    Update:
    Added:
    - Left and right movement

---
+++ Scenes/Entity/Player/Script/Movement.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+func Left_And_Right():
+	var direction = 0
+	
+	if Input.is_action_pressed("Left"):
+		direction -= 1
+	if Input.is_action_pressed("Right"):
+		direction += 1
+	
+	return direction

29efdd4ff262538fc97669460452014e138d74f5
Francis Leo <drypasta34@gmail.com>
2025-05-19 19:08:46 +0800

    Update
    Added:
    
    Fixed:
    - Make the code more readable and clean

--- Scenes/Collectibles/Coin/coin_spawner.gd
+++ Scenes/Collectibles/Coin/coin_spawner.gd
@@ -1,68 +1,32 @@
 extends Node2D
 
-var coinScene = preload("res://Scenes/Collectibles/Coin/coin.tscn")
-var x = 3 #Min Interval in seconds
-var y = 20 #Max Interval in seconds
+const coinScene = preload("res://Scenes/Collectibles/Coin/coin.tscn")
+const minInterval = 3
+const maxInterval = 20
+const coinCount = 10
 
 func _ready() -> void:
-	RandomTimer($SpawnerCooldown)
-	RandomTimer($SpawnerCooldown2)
-	RandomTimer($SpawnerCooldown3)
-	RandomTimer($SpawnerCooldown4)
-	RandomTimer($SpawnerCooldown5)
-	RandomTimer($SpawnerCooldown6)
-	RandomTimer($SpawnerCooldown7)
-	RandomTimer($SpawnerCooldown8)
-	RandomTimer($SpawnerCooldown9)
-	RandomTimer($SpawnerCooldown10) 
 	
-func RandomTimer(timer: Timer):
-	var delay = randi_range(x, y)
-	timer.wait_time = delay
+	randomize()
+	
+	for i in coinCount:
+		var timer = Timer.new()
+		add_child(timer)
+		timer.timeout.connect(func():
+			_SpawnCoin()
+			_RandomizeTimer(timer)
+		)
+
+		_RandomizeTimer(timer)
+
+func _RandomizeTimer(timer: Timer):
+	timer.wait_time = randi_range(minInterval, maxInterval)
 	timer.start()
 	
-func CoinSpawner():
+func _SpawnCoin():
 	var coin = coinScene.instantiate()
-	var randomX = randf_range(65, 1089)
-	coin.position = Vector2(randomX, -65)
+	coin.position = Vector2(randf_range(65, 1089), -65)
 	add_child(coin)
 
-func _on_spawner_cooldown_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown)
-
-func _on_spawner_cooldown_2_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown2)
-
-func _on_spawner_cooldown_3_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown3)
-
-func _on_spawner_cooldown_4_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown4)
-
-func _on_spawner_cooldown_5_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown5)
-
-func _on_spawner_cooldown_6_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown6)
-
-func _on_spawner_cooldown_7_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown7)
-
-func _on_spawner_cooldown_8_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown8)
-
-func _on_spawner_cooldown_9_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown9)
-
-func _on_spawner_cooldown_10_timeout() -> void:
-	CoinSpawner()
-	RandomTimer($SpawnerCooldown10)
+func _process(delta: float) -> void:
+	print($".".get_child_count())
